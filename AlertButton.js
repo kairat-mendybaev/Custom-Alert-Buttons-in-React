606810c76ec95bac6db6c0376c1b8c8193ce7e45
Kairat Mendybaev <113317646+kairat-mendybaev@users.noreply.github.com>
2025-03-13 23:26:57 -0700

    Update AlertButton.js

--- AlertButton.js
+++ AlertButton.js
@@ -1,1 +1,14 @@
+import React from 'react';
+
+function AlertButton({ message, children }) {
+    const handleClick = () => alert(message);
+
+    return (
+        <button onClick={handleClick}>
+            {children}
+        </button>
+    );
+}
+
+export default AlertButton;
 
