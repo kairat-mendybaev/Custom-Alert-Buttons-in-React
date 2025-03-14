01c5268f97c697f934dd9b0ca4142d90700474df
Kairat Mendybaev <113317646+kairat-mendybaev@users.noreply.github.com>
2025-03-13 23:27:43 -0700

    Update Toolbar.js

--- Toolbar.js
+++ Toolbar.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import AlertButton from './AlertButton';
+
+function Toolbar() {
+    return (
+        <div>
+            <AlertButton message="Hello World!">Click Me!</AlertButton>
+            <AlertButton message="Another message!">Another Button</AlertButton>
+        </div>
+    );
+}
+
+export default Toolbar;
 
